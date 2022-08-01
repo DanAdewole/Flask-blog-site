fd895c8aaf276b782a382b64415c0b4e4e05d113
DanAdewole <50157693+DanAdewole@users.noreply.github.com>
2022-08-01 19:40:42 +0100

    linked the title to open in a new page

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from flask import Flask, render_template
 import requests
+from post import Post
 
 app = Flask(__name__)
+
+post_objects = []
 
 blog_url = "https://api.npoint.io/e3945d59591dd324411e"
 response = requests.get(blog_url)
 all_posts = response.json()
 
+for blog_post in all_posts:
+	post = Post(id=blog_post['id'], title=blog_post['title'], subtitle=blog_post['subtitle'], detail=blog_post['detail'])
+	post_objects.append(post)
+
 
 @app.route('/')
 # @app.route('/index')
 def home_page():
-	return render_template("index.html", posts=all_posts)
+	return render_template("index.html", posts=post_objects)
 
 
 @app.route('/about')
@@ -22,6 +29,16 @@ def about_page():
 @app.route('/contact')
 def contact_page():
 	return render_template("contact.html")
+
+
+@app.route('/post/<int:id>')
+def post_page(id):
+	post_to_render = None
+	for post in post_objects:
+		if post.id == id:
+			post_to_render = post
+	print(post_to_render)
+	return render_template("post.html", post=post_to_render)
 
 
 if __name__ == "__main__":
